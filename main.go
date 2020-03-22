e62a39b7d04a422603b55b989559b26dc95fa4dc
BrunoDM2943 <bdm2943@gmail.com>
2020-03-22 16:06:06 -0300

    Adding local environment

--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"github.com/BrunoDM2943/church-members-api/handler"
 	"github.com/BrunoDM2943/church-members-api/handler/filters"
+	_ "github.com/BrunoDM2943/church-members-api/infra/config"
 	mongo2 "github.com/BrunoDM2943/church-members-api/infra/mongo"
 	"github.com/BrunoDM2943/church-members-api/member/repository"
 	"github.com/BrunoDM2943/church-members-api/member/service"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	r := gin.Default()
-	auth := filters.NewAuthFilter()
-	r.Use(auth.Validate())
 	mongo := mongo2.NewMongoConnection()
 	con := mongo.Connect()
+
+	r := gin.Default()
+
+	if viper.GetBool("auth.enable") {
+		auth := filters.NewAuthFilter()
+		r.Use(auth.Validate())
+	}
 
 	membersService := service.NewMemberService(repository.NewMemberRepository(con))
 

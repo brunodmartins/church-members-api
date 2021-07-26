da97a765f3003069c627512cf3fc8e5cca2c677b
BrunoDM2943 <bdm2943@gmail.com>
2021-07-26 10:57:15 -0300

    More tests

--- internal/storage/file/pdf_test.go
+++ internal/storage/file/pdf_test.go
@@ -50,3 +50,68 @@ func TestBuildFile(t *testing.T) {
 	assert.NotNil(t, out)
 	assert.Nil(t, err)
 }
+
+func TestBuildFileWithEmptyPhones(t *testing.T) {
+	dtNascimento, _ := time.Parse("2006/01/02", "2020/06/07")
+	dtCasamento, _ := time.Parse("2006/01/02", "2019/09/14")
+	data := []*model.Member{
+		{
+			Person: model.Person{
+				FirstName:    "Test",
+				LastName:     "test test",
+				BirthDate:    &dtNascimento,
+				MarriageDate: &dtCasamento,
+				SpousesName:  "Test spuse",
+				Contact: model.Contact{
+					Email:         "teste@test.com",
+				},
+				Address: model.Address{
+					District: "9",
+					City:     "Does not sleep",
+					State:    "My-State",
+					Address:  "XXXXX",
+					Number:   9,
+				},
+			},
+		},
+	}
+
+	pdfBuilder := NewPDFBuilder()
+	out, err := pdfBuilder.BuildFile("Test", data)
+	assert.False(t, utf8.Valid(out))
+	assert.NotNil(t, out)
+	assert.Nil(t, err)
+}
+
+func TestBuildFileWithOneHundredMembers(t *testing.T) {
+	dtNascimento, _ := time.Parse("2006/01/02", "2020/06/07")
+	dtCasamento, _ := time.Parse("2006/01/02", "2019/09/14")
+	member := &model.Member{
+		Person: model.Person{
+			FirstName:    "Test",
+			LastName:     "test test",
+			BirthDate:    &dtNascimento,
+			MarriageDate: &dtCasamento,
+			SpousesName:  "Test spuse",
+			Contact: model.Contact{
+				Email: "teste@test.com",
+			},
+			Address: model.Address{
+				District: "9",
+				City:     "Does not sleep",
+				State:    "My-State",
+				Address:  "XXXXX",
+				Number:   9,
+			},
+		},
+	}
+	var data []*model.Member
+	for i:=0;i<100;i++ {
+		data = append(data, member)
+	}
+	pdfBuilder := NewPDFBuilder()
+	out, err := pdfBuilder.BuildFile("Test", data)
+	assert.False(t, utf8.Valid(out))
+	assert.NotNil(t, out)
+	assert.Nil(t, err)
+}

f4a106fbb8ed64a05c9cd5a9aa6c85270634e30f
Bruno <bdm2943@gmail.com>
2021-08-09 13:31:39 -0300

    Adding job application type

--- cmd/application.go
+++ cmd/application.go
@@ -1,7 +1,8 @@
 package cmd
 
 import (
-	config2 "github.com/BrunoDM2943/church-members-api/platform/config"
+	"github.com/BrunoDM2943/church-members-api/platform/config"
+	"github.com/spf13/viper"
 )
 
 //Application interface defines a single run method to be executed
@@ -12,7 +13,10 @@ type Application interface {
 
 //ProvideRunner defines which Application runner should be initialized
 func ProvideRunner() Application {
-	if config2.IsAWS() {
+	if config.IsAWS() {
+		if viper.Get("application") == "JOB" {
+			return JobApplication{}
+		}
 		return LambdaApplication{}
 	} else {
 		return FiberApplication{}
---
+++ cmd/job.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/BrunoDM2943/church-members-api/internal/modules/jobs"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type JobApplication struct{}
+
+func (JobApplication) Run() {
+	lambda.Start(func(payload []byte) error {
+		var input map[string]interface{}
+		err := json.Unmarshal(payload, input)
+		if err != nil {
+			return err
+		}
+		jobType, err := new(jobs.JobType).From(input["name"].(string))
+		if err != nil {
+			return err
+		}
+		job := jobs.BuildJob(jobType)
+		if job == nil {
+			return errors.New("job not found")
+		}
+		return job.RunJob()
+	})
+}

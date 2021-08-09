6fa924da1d2b75dfa0cdbf596832da1c7b1c1c7a
Bruno <bdm2943@gmail.com>
2021-08-09 13:30:27 -0300

    Adding new configurations

--- platform/config/runtime.go
+++ platform/config/runtime.go
@@ -8,12 +8,15 @@ import (
 //InitConfiguration loads global configuration variables on Viper
 func InitConfiguration() {
 	viper.Set("cloud", os.Getenv("SERVER"))
+	viper.Set("application", os.Getenv("APPLICATION"))
 	viper.Set("church.name", os.Getenv("CHURCH_NAME"))
+	viper.Set("church.shortname", os.Getenv("CHURCH_NAME_SHORT"))
 	viper.Set("lang", os.Getenv("APP_LANG"))
 	viper.Set("bundles.location", "bundles")
 	viper.Set("tables.member", os.Getenv("TABLES_MEMBER"))
 	viper.Set("tables.member_history", os.Getenv("TABLES_MEMBER_HISTORY"))
 	viper.Set("pdf.font.path", "./fonts/Arial.ttf")
+	viper.Set("jobs.daily.phones", os.Getenv("JOBS_DAILY_PHONE"))
 }
 
 func IsAWS() bool {

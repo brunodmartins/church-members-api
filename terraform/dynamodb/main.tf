eb01958b5403b1c008b23693706fbeb63eb34715
BrunoDM2943 <bdm2943@gmail.com>
2021-10-23 22:01:39 -0300

    Adding church table

--- terraform/dynamodb/main.tf
+++ terraform/dynamodb/main.tf
@@ -37,6 +37,19 @@ resource "aws_dynamodb_table" "user_table" {
   }
 }
 
+resource "aws_dynamodb_table" "church_table" {
+  name           = "church"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
 output "member_table_name" {
   value = aws_dynamodb_table.member_table.name
 }
@@ -50,6 +63,9 @@ output "user_table_name" {
 }
 
 output "tables_arn" {
-  value = [aws_dynamodb_table.member_table.arn, aws_dynamodb_table.member_history_table.arn, aws_dynamodb_table.user_table.arn]
+  value = [
+    aws_dynamodb_table.member_table.arn,
+    aws_dynamodb_table.member_history_table.arn,
+  aws_dynamodb_table.user_table.arn]
 }
 

e24c4215739605f85fdc1a0a7b3b824147113fcd
BrunoDM2943 <bdm2943@gmail.com>
2021-11-03 22:28:24 -0300

    Adding new field maritalStatus

--- playbook/migrate_table.sh
+++ playbook/migrate_table.sh
@@ -3,5 +3,11 @@ items=($(aws dynamodb scan --table-name member | jq '.Items[] | .id["S"]'))
 for id in "${items[@]}"
 do
   item=$(aws dynamodb get-item --table-name member --key "{\"id\":{\"S\":${id}}}" | jq -r '.Item')
-  aws dynamodb put-item --table-name member_v3 --item "$item"
+  maritalStatus="MARRIED"
+  single=($(echo "$item" | jq '. | {marriageDateShort}' | grep '"NULL": true'))
+  if [ -n "$single" ]; then
+    maritalStatus="SINGLE"
+  fi
+  item=`echo "$item" | jq --arg status "$maritalStatus" '. + {"maritalStatus":{"S":$status}}'`
+  aws dynamodb put-item --table-name member_v2 --item "$item"
 done

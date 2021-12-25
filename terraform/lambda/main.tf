b98fd2510d7d3cc3c8b2f9809dc08e24bbb5fef9
BrunoDM2943 <bdm2943@gmail.com>
2021-12-25 17:09:43 -0300

    fixing function name

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -99,3 +99,7 @@ output "lambda_name" {
 output "lambda_job_arn" {
   value = aws_lambda_function.lambda_job.arn
 }
+
+output "lambda_job_name" {
+  value = aws_lambda_function.lambda_job.function_name
+}

bf5efad4bfa7042783415a6b110e5a9f7af92a5b
BrunoDM2943 <bdm2943@gmail.com>
2021-12-27 21:18:54 -0300

    Fixing public bucket

--- terraform/s3/s3.tf
+++ terraform/s3/s3.tf
@@ -23,6 +23,14 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+
 output "bucket_name" {
     value = "church-members-data"
 }

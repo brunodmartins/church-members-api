f0f3462c7575fdfbdb5e297920605e8f2dd4afd6
brunodmartins <bdm2943@icloud.com>
2025-07-14 22:52:39 -0300

    Adding a new login mechanism
    Now it supports receiving the header 'church_abbreviation,' to make ir a more fluid experience to log in via web application

--- internal/modules/church/service.go
+++ internal/modules/church/service.go
@@ -1,11 +1,18 @@
 package church
 
-import "github.com/brunodmartins/church-members-api/internal/constants/domain"
+import (
+	"fmt"
+	"github.com/brunodmartins/church-members-api/internal/constants/domain"
+	apierrors "github.com/brunodmartins/church-members-api/platform/infra/errors"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
 
 //go:generate mockgen -source=./service.go -destination=./mock/service_mock.go
 type Service interface {
 	List() ([]*domain.Church, error)
 	GetChurch(id string) (*domain.Church, error)
+	GetChurchByAbbreviation(abbreviation string) (*domain.Church, error)
 }
 
 type churchService struct {
@@ -22,4 +29,20 @@ func (s churchService) List() ([]*domain.Church, error) {
 
 func (s churchService) GetChurch(id string) (*domain.Church, error) {
 	return s.repo.GetByID(id)
+}
+
+func (s churchService) GetChurchByAbbreviation(abbreviation string) (*domain.Church, error) {
+	churches, err := s.repo.List()
+	if err != nil {
+		logrus.Errorf("error looking for church by abbreviation %s: %v", abbreviation, err)
+		return nil, err
+	}
+	for _, ch := range churches {
+		if ch.Abbreviation == abbreviation {
+			return ch, nil
+		}
+	}
+	err = apierrors.NewApiError(fmt.Sprintf("achurch for bbreviation %s not found", abbreviation), http.StatusNotFound)
+	logrus.Infof("church for abbreviation %s not found", abbreviation)
+	return nil, err
 }
--- runbook/create_local_data.sh
+++ runbook/create_local_data.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-aws dynamodb put-item --table-name church_dev --item "$(cat runbook/local_church.json)" --endpoint http://127.0.0.1:4566
-aws dynamodb put-item --table-name user_dev --item "$(cat runbook/local_user.json)"  --endpoint http://127.0.0.1:4566
+aws dynamodb put-item --table-name church_dev --item "$(cat local_church.json)" --endpoint http://127.0.0.1:4566
+aws dynamodb put-item --table-name user_dev --item "$(cat local_user.json)"  --endpoint http://127.0.0.1:4566
+
+echo "
+Church Abbreviation LOCAL
+Login test_user
+Password 1234567890
+"

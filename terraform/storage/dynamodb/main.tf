e66bb1f977454e8e6daccf2e9689ffc9b84fa97e
brunodmartins <bdm2943@icloud.com>
2026-01-17 18:19:48 -0300

    Adding new Terraform table

--- terraform/storage/dynamodb/main.tf
+++ terraform/storage/dynamodb/main.tf
@@ -113,3 +113,22 @@ resource "aws_dynamodb_table" "church_table" {
   }
 }
 
+resource "aws_dynamodb_table" "participant" {
+  name           = var.participant_table_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "name-index"
+    hash_key           = "name"
+    projection_type    = "ALL"
+  }
+}
